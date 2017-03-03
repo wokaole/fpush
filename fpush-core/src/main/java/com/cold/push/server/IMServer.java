805cc913c4aa84bd6d336723e193c84dad8f3bf8
wenhui_liao <wenhui_liao@kingdee.com>
2017-03-03 19:01:19 +0800

    init package

---
+++ fpush-core/src/main/java/com/cold/push/server/IMServer.java
@@ -0,0 +1,8 @@
+package com.cold.push.server;
+
+/**
+ * @author liaowenhui
+ * @date 2017/3/3 19:00.
+ */
+public class IMServer {
+}
---
+++ fpush-core/src/test/java/com/cold/push/Main.java
@@ -0,0 +1,8 @@
+package com.cold.push;
+
+/**
+ * @author liaowenhui
+ * @date 2017/3/3 19:01.
+ */
+public class Main {
+}
